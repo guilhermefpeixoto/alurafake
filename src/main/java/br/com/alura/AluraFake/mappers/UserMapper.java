c4e249d7fc58d814ce17927d67e204b2fe2b3e69
guilhermefpeixoto <guilherme.peixoto@ccc.ufcg.edu.br>
2025-06-06 17:34:21 -0300

    feat: creates a mapper to user and user dtos

---
+++ src/main/java/br/com/alura/AluraFake/mappers/UserMapper.java
@@ -0,0 +1,20 @@
+package br.com.alura.AluraFake.mappers;
+
+import br.com.alura.AluraFake.dtos.NewUserDTO;
+import br.com.alura.AluraFake.dtos.UserResponseDTO;
+import br.com.alura.AluraFake.models.User;
+
+public class UserMapper {
+
+    public static User toEntity(NewUserDTO userDTO) {
+        User user = new User(userDTO.name(), userDTO.email(), userDTO.role(), userDTO.password());
+
+        return user;
+    }
+
+    public static UserResponseDTO toDTO(User user) {
+        UserResponseDTO userDTO = new UserResponseDTO(user.getName(), user.getEmail(), user.getRole());
+        
+        return userDTO;
+    }
+}
