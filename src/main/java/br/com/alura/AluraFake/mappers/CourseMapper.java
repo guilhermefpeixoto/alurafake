3fb11ce925bc1867ff64123788a62e05af8ad8ad
guilhermefpeixoto <guilherme.peixoto@ccc.ufcg.edu.br>
2025-06-07 14:44:36 -0300

    feat: creates course mapper

---
+++ src/main/java/br/com/alura/AluraFake/mappers/CourseMapper.java
@@ -0,0 +1,20 @@
+package br.com.alura.AluraFake.mappers;
+
+import br.com.alura.AluraFake.dtos.CourseResponseDTO;
+import br.com.alura.AluraFake.dtos.NewCourseDTO;
+import br.com.alura.AluraFake.models.Course;
+import br.com.alura.AluraFake.models.User;
+
+public class CourseMapper {
+    public static Course toEntity(NewCourseDTO courseDTO, User instructor) {
+        Course course = new Course(courseDTO.title(), courseDTO.description(), instructor);
+
+        return course;
+    }
+
+    public static CourseResponseDTO toDTO(Course course) {
+        CourseResponseDTO courseDTO = new CourseResponseDTO(course.getId(), course.getTitle(), course.getDescription(), course.getStatus());
+        
+        return courseDTO;
+    }
+}
