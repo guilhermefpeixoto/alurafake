35686c9b8ab1b7f2644e537b1f031b446f5cec3a
guilhermefpeixoto <guilherme.peixoto@ccc.ufcg.edu.br>
2025-06-08 22:03:30 -0300

    refactor: renames column 'option' in table task option

--- src/main/resources/db/migration/V4__createTableTaskOption.sql
+++ src/main/resources/db/migration/V4__createTableTaskOption.sql
@@ -2,7 +2,7 @@ CREATE TABLE Task_Option (
     id bigint(20) NOT NULL AUTO_INCREMENT,
     createdAt datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
     task_id bigint(20) NOT NULL,
-    option_text varchar(80) NOT NULL,
+    `option` varchar(80) NOT NULL,
     isCorrect tinyint(1) NOT NULL,
     PRIMARY KEY (id),
     CONSTRAINT FK_Task FOREIGN KEY (task_id) REFERENCES Task(id) ON DELETE CASCADE
